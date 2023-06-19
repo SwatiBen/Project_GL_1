ef747a69b8c755d4248adf769f29ae293d0028b0
Swati Beniwal <101699580+SwatiBen@users.noreply.github.com>
2023-06-19 23:36:29 +0530

    some changes made

--- Graded_Project_1/src/com/ben/driverPackage/Driver.java
+++ Graded_Project_1/src/com/ben/driverPackage/Driver.java
@@ -8,30 +8,19 @@ public class Driver {
 	public static void main(String[] args) {
 
 		AdminDepartment adminObj = new AdminDepartment();
-		System.out.println("Welcome to "+ 
-				adminObj.departmentName() + "\n" + 
-				adminObj.getTodaysWork() + "\n" +
-				adminObj.getWorkDeadline() + "\n" + 
-				adminObj.isTodayAHoliday()
-				);
-		System.out.printf("\n\n");
+		System.out.println("Welcome to " + adminObj.departmentName() + "\n" + adminObj.getTodaysWork() + "\n"
+				+ adminObj.getWorkDeadline() + "\n" + adminObj.isTodayAHoliday());
+		System.out.printf("\n");
 
 		HrDepartment hrObj = new HrDepartment();
-		System.out.printf("Welcome to " + 
-				hrObj.departmentName() + "\n" +
-				hrObj.doActivity() + "\n" +
-				hrObj.getTodaysWork() + "\n" +
-				hrObj.getWorkDeadline() + "\n" +
-				hrObj.isTodayAHoliday());
+		System.out.printf("Welcome to " + hrObj.departmentName() + "\n" + hrObj.doActivity() + "\n"
+				+ hrObj.getTodaysWork() + "\n" + hrObj.getWorkDeadline() + "\n" + hrObj.isTodayAHoliday());
 		System.out.printf("\n\n");
 
 		TechDepartment techObj = new TechDepartment();
-		System.out.printf("Welcome to " +
-				techObj.departmentName() + "\n" +
-				techObj.getTodaysWork() + "\n" +
-				techObj.getWorkDeadline() + "\n" +
-				techObj.getTechStackInformation() + "\n" +
-				techObj.isTodayAHoliday());
+		System.out.printf("Welcome to " + techObj.departmentName() + "\n" + techObj.getTodaysWork() + "\n"
+				+ techObj.getWorkDeadline() + "\n" + techObj.getTechStackInformation() + "\n"
+				+ techObj.isTodayAHoliday());
 
 	}
 
