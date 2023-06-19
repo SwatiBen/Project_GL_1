3441e156e77b01d2bf05807e639b8236e27794f7
Swati Beniwal <101699580+SwatiBen@users.noreply.github.com>
2023-06-19 23:47:33 +0530

    Update HrDepartment.java

--- Graded_Project_1/src/com/ben/departmentsPackage/HrDepartment.java
+++ Graded_Project_1/src/com/ben/departmentsPackage/HrDepartment.java
@@ -4,17 +4,14 @@ import com.ben.superDepartment.SuperDepartment;
 
 public class HrDepartment extends SuperDepartment {
 
-	// OverRidding ParentMethod
 	public String departmentName() {
 		return "HR Department";
 	}
 
-	// OverRidding ParentMethod
 	public String getTodaysWork() {
 		return "Fill today’s timesheet and mark your attendance";
 	}
 
-	// OverRidding ParentMethod
 	public String getWorkDeadline() {
 		return "Complete by EOD";
 	}
